4f27efefdeb61843825a623d46de40dd6b2ca0a2
shilpakancharla <shilpakancharla18@gmail.com>
2021-04-16 12:59:18 -0400

    Hyperparameter search

--- dqn.py
+++ dqn.py
@@ -14,33 +14,67 @@ np.random.seed(0)
     Implementation of Deep Q-Learning Network (DQN).
 """
 class DeepQLearning:
-    def __init__(self, action_options, state_space):
-        self.action_options = action_options
+    """
+        @action_space: Four discrete actions are available: do nothing, fire left orientation engine, 
+                       fire main engine, fire right orientation engine.
+        @state_space: State refers to position of the lunar lander. Landing site has coordinates (0, 0).
+        @batch_size: Determines the batch size.
+        @epsilon_min: Minimum allowed epsilon value. Determines if epsilon decay is factored in.
+        @epsilon: Randomness factor which forces us to try different actions.
+        @gamma: Discount factor which quantifies how much importance we give for future rewards.
+        @learning_rate: Determines the step size at each iteration while moving toward a minimum of a loss function.
+        @epsilon_decay: Factor by which epsilon decreases.
+        @record: List to keep track of state, reward, action, the next state, and if we have reach a terminal state.
+        @model: Calls to define the agent model for DQN.
+
+        We conduct a search of the best hyperparameters based on the epsilon, gamma, learning rate, and epsilon decay.
+        We initialize a model that takes into account these hyperparameters.
+    """
+    def __init__(self, action_space, state_space, epsilon, gamma, learning_rate, epsilon_decay):
+        self.action_space = action_space
         self.state_space = state_space
-        self.epsilon = 1.0
-        self.gamma = 0.99
         self.batch_size = 64
         self.epsilon_min = 0.01
-        self.learning_rate = 0.001
-        self.epsilon_decay = 0.996
-        self.record = [None] * 1000000
+        self.epsilon = epsilon
+        self.gamma = gamma
+        self.learning_rate = learning_rate
+        self.epsilon_decay = epsilon_decay
+        self.record = [None] * 1000000 # Create a new record for every combination of parameters
         self.model = self.define_agent_model()
 
+    """
+        Defining the agent's neural network.
+
+        @return compiled neural network model
+    """
     def define_agent_model(self):
         model = Sequential()
-        model.add(Dense(150, input_dim = self.state_space, activation = 'relu'))
-        model.add(Dense(120, activation = 'relu'))
-        model.add(Dense(self.action_options, activation = 'linear'))
+        model.add(Dense(125, input_dim = self.state_space, activation = 'relu'))
+        model.add(Dense(100, activation = 'relu'))
+        model.add(Dense(self.action_space, activation = 'linear'))
         model.compile(loss = 'mse', optimizer = Adam(lr = self.learning_rate))
         return model
 
-    def keep_record(self, state, action, reward, next_state, done_status):
-        self.record.append((state, action, reward, next_state, done_status))
+    """
+        Keeping a record of the state, action, reward, next state, and if we have reached the terminal
+        status.
+
+        @param state: current state
+        @param action: action taken
+        @param reward: reward earned from action taken
+        @param next state: next state in the state space from action taken
+        @param terminal_status: Boolean flag to see if we have reached the terminal state
+    """
+    def keep_record(self, state, action, reward, next_state, terminal_status):
+        self.record.append((state, action, reward, next_state, terminal_status))
     
+    """
+        
+    """
     def act(self, state):
         # Exploration-exploitation
         if np.random.rand() <= self.epsilon:
-            return random.randrange(self.action_options)
+            return random.randrange(self.action_space)
         act_values = self.model.predict(state)
         return np.argmax(act_values[0])
 
@@ -53,58 +87,80 @@ class DeepQLearning:
         actions = np.array([i[1] for i in mini_batch])
         rewards = np.array([i[2] for i in mini_batch])
         next_states = np.array([i[3] for i in mini_batch])
-        done_status = np.array([i[4] for i in mini_batch])
+        terminal_status = np.array([i[4] for i in mini_batch])
 
         states = np.squeeze(states) # Remove axes of length one from states
         next_states = np.squeeze(next_states) # Remove axes of length one from next_states
 
-        targets = rewards + self.gamma * (np.amax(self.model.predict_on_batch(next_states), axis = 1)) * (1 - done_status)
+        targets = rewards + self.gamma * (np.amax(self.model.predict_on_batch(next_states), axis = 1)) * (1 - terminal_status)
         targets_full = self.model.predict_on_batch(states)
         idx = np.array([i for i in range(self.batch_size)])
         targets_full[[idx], [actions]] = targets
 
         self.model.fit(states, targets_full, epochs = 1, verbose = 0)
+        # If the current epsilon value is greater than the minimum allowed epsilon value, decay the value
+        # If the current epsilon value is less than the minimum already, do not decay it further
         if self.epsilon > self.epsilon_min:
             self.epsilon *= self.epsilon_decay
 
 def train_dqn(episode):
-    loss = []
-    agent = DeepQLearning(env.action_options.n, env.observation_space.shape[0])
-    for ep in range(episode):
-        state = env.reset()
-        state = np.reshape(state, (1, 8))
-        score = 0
-        max_steps = 2000
-        for i in range(max_steps):
-            action = agent.act(state)
-            env.render()
-            next_state, reward, done_status, _ = env.step(action)
-            score += reward
-            next_state = np.reshape(next_state, (1, 8))
-            agent.keep_record(state, action, reward, next_state, done_status)
-            state = next_state
-            agent.replay()
-            if done_status: # Status is true
-                f = open("log_dqn.txt", "a")
-                f.write("\nEpisode: {}/{}, Score: {}".format(ep, episode, score))
+    # Possible hyperparameter values to search from
+    epsilon_values = [1.0, 0.5, 0.3, 0.1]
+    gamma_values = [1.0, 0.5, 0.3, 0.1]
+    learning_rate_values = [0.1, 0.01, 0.001, 0.0001]
+    epsilon_decay_values = [0.999, 0.995, 0.900]
+    for ev, gv, lrv, edv in zip(epsilon_values, gamma_values, learning_rate_values, epsilon_decay_values):
+        loss = [] # Create new array that will hold the loss value
+        reward_record = [] # Create new array that will hold the rewards earned
+        agent = DeepQLearning(env.action_space.n, env.observation_space.shape[0], ev, gv, lrv, edv) # Define the agent parameters
+        for ep in range(episode):
+            state = env.reset()
+            state = np.reshape(state, (1, 8))
+            score = 0
+            max_steps = 2000
+            for i in range(max_steps):
+                action = agent.act(state)
+                env.render()
+                next_state, reward, terminal_status, _ = env.step(action)
+                score += reward
+                next_state = np.reshape(next_state, (1, 8))
+                agent.keep_record(state, action, reward, next_state, terminal_status)
+                state = next_state
+                agent.replay()
+                if terminal_status: # If we have reach the terminal state
+                    f = open("log_files\log_dqn_{}_{}_{}_{}.txt".format(ev, gv, lrv, edv), "a")
+                    f.write("\nEpisode: {}/{}, Score: {}".format(ep, episode, score))
+                    f.close()
+                    break
+            loss.append(score)
+            reward_record.append(reward)
+            is_solved = np.mean(loss[-100:]) # Average score of last 100 episodes
+            if is_solved > 200:
+                f = open("log_files\log_dqn_{}_{}_{}_{}.txt".format(ev, gv, lrv, edv), "a")
+                f.write('\nTask completed.\n')
                 f.close()
                 break
-        loss.append(score)
-        is_solved = np.mean(loss[-100:]) # Average score of last 100 episodes
-        if is_solved > 200:
-            f = open("log_dqn.txt", "a")
-            f.write('\nTask completed.\n')
+            f = open("log_files\log_dqn_{}_{}_{}_{}.txt".format(ev, gv, lrv, edv), "a")
+            f.write("\nAverage of last 100 episodes: {0:.2f}\n".format(is_solved))
             f.close()
-            break
-        f = open("log_dqn.txt", "a")
-        f.write("\nAverage of last 100 episodes: {0:.2f}\n".format(is_solved))
-        f.close()
+    
+        # Create a plot for the loss in the trial
+        plt.plot([i + 1 for i in range(0, len(loss), 2)], loss[::2])
+        plt.title("Loss for Epsilon = {}, Gamma = {}, Learning Rate = {}, Epsilon Decay = {}".format(ev, gv, lrv, edv))
+        plt.xlabel("Steps")
+        plt.ylabel("Loss")
+        plt.savefig("figures/loss/loss_{}_{}_{}_{}.png".format(ev, gv, lrv, edv))
+
+        # Create a plot for the reward in the trial
+        plt.plot([i + 1 for in range(0, len(reward_record), 2)], reward_record[::2])
+        plt.title("Reward for Epsilon = {}, Gamma = {}, Learning Rate = {}, Epsilon Decay = {}".format(ev, gv, lrv, edv))
+        plt.xlabel("Steps")
+        plt.ylabel("Reward")
+        plt.savefig("figures/rewards/reward_{}_{}_{}_{}.png".format(ev, gv, lrv, edv))
     return loss
 
 if __name__ == '__main__':
     print(env.observation_space)
-    print(env.action_options)
+    print(env.action_space)
     episodes = 400
     loss = train_dqn(episodes)
-    plt.plot([i + 1 for i in range(0, len(loss), 2)], loss[::2])
-    plt.show()
